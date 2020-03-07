c40e98c2c7b7bb07aafd61669ba5010e4c7135dc
Marlon Menegatti <devmenegatti@gmail.com>
2020-03-07 01:37:29 -0300

    Configuração de model e controller de Sessões

---
+++ src/controllers/SessionController.js
@@ -0,0 +1,27 @@
+//metodos: index, show, update, store, destroy
+/*
+index: listagem de sessoes
+store: Criar sessao
+show: Quando queremos listar uma UNICA sessao
+update: quando queremos alterar alguma sessao
+destroy: quando queremos deletar uma sessão
+*/
+
+import User from '../models/User';
+
+class SessionController{
+
+    async store(req, res){
+        const { email } = req.body; 
+
+        let user = await User.findOne({ email });
+
+        if (!user) {
+            user = await User.create({ email });
+        }
+
+        return res.json(user);
+    }
+}
+
+export default new SessionController();
