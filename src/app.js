17cd5ac92e36eda70bbca8dae8d050d5ab7cab9a
Marlon Menegatti <devmenegatti@gmail.com>
2020-03-07 02:24:36 -0300

    Upload de foto em armazenamento local

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import mongoose from 'mongoose';
 import routes from './routes';
 
@@ -16,6 +17,10 @@ class App{
 	}
 
 	middlewares(){
+		this.server.use(
+			'/files',
+			express.static(path.resolve(__dirname, '..', 'uploads'))
+		);
 		this.server.use(express.json());
 
 	}
--- src/models/House.js
+++ src/models/House.js
@@ -10,6 +10,14 @@ const HouseSchema = Schema({
         type: Schema.Types.ObjectId,
         ref: 'User'
     }
+}, {
+    toJSON: {
+        virtuals: true
+    }
+});
+
+HouseSchema.virtual('thumbnail_url').get(function() {
+    return `http://localhost:3333/files/${this.thumbnail}`;
 });
 
 export default model('House', HouseSchema);
